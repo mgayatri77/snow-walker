d0ccf3db38376a43208a0bb05e4b691d783ed559
Mohit Gayatri <mohitgayatri96@gmail.com>
2022-12-12 08:00:25 +0530

    update instructions

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ $team = "Checkmate";
 $instruction = <<<EOD
 <p> Welcome to Grid City! </p>
 <p> Grid City is a small planned city laid out as a grid with streets going north-south and east-west. There is one building per city block. The grid has suffered a snow storm and the Snow Clearing Department (GridClear) wishes to make it possible to reach each city block. </p> 
-<p>The head of GridClear consults you to help plan the plow paths such that for any two adjacent buildings a resident will need to travel over only a few streets/buildings.The "score" of a path is the worst case, i.e. the largest number of such streets/buildings.</p>
+<p>The head of GridClear consults you to help plan the plow paths such that for any two adjacent (north, south, east, west, and any diagonal) buildings, a resident will need to travel over only a few streets/buildings. The "score" of a building is the worst case, i.e. the largest number of such streets/buildings required to go from that building to any adjacent building.</p>
 
 </p> <strong> Rules </strong>
     <p> - Setttings: GridX and GridY must be greater than 1. 
@@ -15,9 +15,9 @@ $instruction = <<<EOD
     <p> - Paths: A small number of paths may have already been cleared by GridClear and will be marked in black and yellow. </p>
     <p> - Paths: A plow path must begin from the boundaries of the grid. </p>
     <p> - Paths: A plow path cannot go over previously plowed paths. </p> 
-    <p> - Paths: All plowed paths must be connected. </p>
+    <p> - Paths: Each single plow path must be connected. </p>
     <p> - Scoring: Crossing a plowed intersection has zero cost. </p>
-    <p> - Scoring: Going diagonally across a building has the same cost as walking along a paved road - 1.</p>
+    <p> - Scoring: Going diagonally across a building has the same cost as walking along a paved road which is 1.</p>
 </p>
 
 <p> <strong> Instructions </strong>
